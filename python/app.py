8069110195f20de199164409e34891c23ba12d70
Matt25969 <matt.jo.hunt@gmail.com>
2019-07-17 11:27:13 +0100

    Added Dockerfile and HTTP requests to Python example

--- python/app.py
+++ python/app.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask, jsonify, make_response
 import sys
+import requests
 from random import randint
 app = Flask(__name__)
 
@@ -13,9 +14,17 @@ def num_gen_minmax(max):
     rand = randint(0,max)
     return jsonify({"Random Number":rand})
 
+@app.route('/anEndpoint')
+def make_request():
+    return requests.get('http://example.com').content
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=9017)
+
+
+
+
