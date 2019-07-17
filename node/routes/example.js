2865dc3f68ad5703eba7466494e35439738efc16
Matt25969 <matt.jo.hunt@gmail.com>
2019-07-17 10:36:31 +0100

    added HTTP Request example in Node

--- node/routes/example.js
+++ node/routes/example.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require('axios');
 
 // @route   GET example/test
 // @desc    Tests route
@@ -20,5 +21,19 @@ router.get("/generate", (req, res) => {
 
       res.send(200,randomNumber);
     })
+
+// @route   GET example/sendingARequest
+// @desc    Sends a HTTP get request to another service using the axios module
+// @access  Public
+router.get("/sendingARequest", (req, res) => {
+
+axios.get('http://127.0.0.1:9000/notify').catch(error => {
+      console.log(error);
+    });
+  
+
+    })
+
+
 
 module.exports = router;
